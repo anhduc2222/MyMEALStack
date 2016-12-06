ce8f3e0cbaeb04499f3eb39fba969e30c419cc63
Duc Vu <duc.vuanh1987@gmail.com>
2016-12-06 10:36:56 +0700

    no message

--- API/build/config/strategies.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var _ = require('lodash');
-var BearStrategy = require('passport-http-bearer');
-var Be = (function () {
-    function Be() {
-    }
-    return Be;
-}());
-//# sourceMappingURL=strategies.js.map
--- API/src/app/models/authorization/TokenModel.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Created by vaduc on 7/25/2016.
- */
-import mongoose = require("mongoose");
-
-interface TokenModel extends mongoose.Document {
-}
-
-export = TokenModel;
